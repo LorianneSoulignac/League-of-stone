917113b6c45331f081cf93c5aec3a485119c5fe7
ayoube.soler <ayoube.soler1@gmail.com>
2019-01-20 01:37:55 +0100

    add functionality eyes and add function matchmaking in Game but erreur in refresh request

--- los-ui-master/src/Game.js
+++ los-ui-master/src/Game.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
+
 // import { Link } from 'react-router-dom';
 
 import Modal from 'react-responsive-modal';
+import { SERVER_URL } from './consts';
 
 class Game extends Component {
-  state = {
-    open: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      token : this.props.location.state.token,
+      id: this.props.location.state.id,
+      name: this.props.location.state.name,
+      joueurs: []
+    };
+    console.log ("token :" + this.state.token);
+    console.log ("id :" + this.state.id);
+    console.log("name : " + this.state.name)
+  }
  
   onOpenModal = () => {
     this.setState({ open: true });
@@ -16,18 +29,50 @@ class Game extends Component {
   onCloseModal = () => {
     this.setState({ open: false });
   };
- 
+  
+  makingMatch(){
+    let tab = []
+    axios
+      .get(
+        SERVER_URL +
+          "/matchmaking/participate?userid="+this.state.id+"&name="+this.state.name+"&token="+this.state.token
+      ).then(res => {
+        if (res.data.status === "ok") { 
+          tab.push({name : this.state.name, userid: this.state.id})
+          
+        }
+        this.setState({joueurs: tab})
+        
+        
+      });
+      
+      
+  }
+
   render() {
-    const { open } = this.state;
-    return (
+    // const { open } = this.state;
+    
+    return(
       <div>
-        <button onClick={this.onOpenModal}>Open modal</button>
+        <button onClick={()=>{this.makingMatch()}}>JOUER</button>
+        <ul>
+          <div>
+            {this.state.joueurs.map((recipe)=>{
+              return <li key={recipe.userid}>{recipe.name}</li>
+            })}
+          </div>
+        {/* <button onClick={this.onOpenModal}>Open modal</button>
         <Modal open={open} onClose={this.onCloseModal} center>
           <h2>Match-making</h2>
-          <div className="requete"></div>
-          <div classeName="searchChampion"></div>
+          <div className="requete">
+            <ul>
+              {this.renderTools()}
+            </ul>
+          </div>
+          <div className="searchChampion"></div>
           <button onClick={this.onCloseModal}>Open modal</button>
-        </Modal>
+        </Modal> */}
+        </ul>
       </div>
     );
   }
--- los-ui-master/src/pages/SigninSignup/Signin.js
+++ los-ui-master/src/pages/SigninSignup/Signin.js
@@ -12,7 +12,8 @@ class Signin extends Component {
     super(props);
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      visibility: false
     };
 
     this.handleChangeEmail = this.handleChangeEmail.bind(this);
@@ -33,7 +34,9 @@ class Signin extends Component {
       .then(res => {
         if (res.data.status === "ok") {
           this.props.setSessionToken(res.data.token);
-          this.props.history.push(process.env.PUBLIC_URL + "/");
+          this.props.history.push({pathname : process.env.PUBLIC_URL + "/",
+        state : {token : res.data.data.token, id: res.data.data.id, name : res.data.data.name}});
+          
         }
       });
   }
@@ -63,13 +66,13 @@ class Signin extends Component {
             <label>
               {/* Mot de passe :{" "} */}
               <input
-                type="password"
+                type={this.state.visibility ? 'text' : 'password'}
                 value={this.state.password}
                 onChange={this.handleChangePassword}
                 placeholder="Mot de passe"
                 className="password"
               />
-              <button className="unmask" type="button" title="Mask/Unmask password to check content">Unmask</button>
+              <button className="unmask" type="button" title="" onClick={()=>{this.setState({visibility : ! this.state.visibility})}}>Unmask</button>
             </label>
           </div>
           <div>
